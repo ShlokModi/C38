7a37ed31607510d59e088e6046c8692653da2e4c
ShlokModi <shlokmodi2008@gmail.com>
2020-06-29 19:50:32 +0530

    images

--- sketch.js
+++ sketch.js
@@ -9,8 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
-
+var car1_img, car2_img, car3_img, car4_img, ground_img, track_img;
+function preload(){
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground_img = loadImage("images/ground.png");
+  track_img = loadImage("images/track.jpg");
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.update(2);
   }
 }
